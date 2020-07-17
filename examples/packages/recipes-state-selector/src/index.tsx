8d0f3a09d556ab36185f33098bf263c9ac80cf57
forehalo <forehalo@gmail.com>
2020-07-17 16:54:57 +0800

    chore: update examples

--- examples/packages/recipes-state-selector/src/index.tsx
+++ examples/packages/recipes-state-selector/src/index.tsx
@@ -12,6 +12,7 @@ function Loading() {
   console.info('Loading render')
   const loading = useModuleState(AppModule, {
     selector: (state) => state.loading,
+    dependencies: [],
   })
   if (loading) {
     return <h1 style={{ color: 'hotpink' }}>I am loading</h1>
@@ -23,6 +24,7 @@ function List() {
   console.info('List render')
   const [list, dispatcher] = useModule(AppModule, {
     selector: (state) => state.list,
+    dependencies: [],
   })
 
   const listNodes = list.map((value) => <li key={value}>{value}</li>)
