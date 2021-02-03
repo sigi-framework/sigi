1369acfcb0b30b7cfb93f2181bbd0a508e4abfd2
LongYinan <longyinan.brooklyn@bytedance.com>
2021-02-03 15:56:01 +0800

    feat(react-router): export createHistoryProviders to register history in SSR

--- packages/react-router/src/index.ts
+++ packages/react-router/src/index.ts
@@ -1,2 +1,26 @@
+import type { Provider } from '@sigi/di'
+import type { History } from 'history'
+import { NEVER, Observable } from 'rxjs'
+
+import { HistoryProvide, Router$Provide, RouterChanged } from './router.module'
+
+export function createHistoryProviders(history: History): [Provider<History>, Provider<Observable<RouterChanged>>] {
+  return [
+    {
+      provide: HistoryProvide.provide,
+      useValue: history,
+    },
+    {
+      provide: Router$Provide.provide,
+      useFactory: () => {
+        if (process.env.NODE_ENV !== 'production') {
+          console.warn('RouterModule.router$ will never emit values nor complete in SSR scenario')
+        }
+        return NEVER
+      },
+    },
+  ]
+}
+
 export { RouterModule } from './router.module'
 export * from './router-provider'
