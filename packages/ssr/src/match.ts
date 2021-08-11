38738ab80d1f9b46baeb80824bb9369bcf118c5a
LongYinan <longyinan.brooklyn@bytedance.com>
2021-08-11 18:23:22 +0800

    fix(ssr): payloadGetter type in match function

--- packages/ssr/src/match.ts
+++ packages/ssr/src/match.ts
@@ -3,7 +3,7 @@ import { match as matchPath } from 'path-to-regexp'
 import { SKIP_SYMBOL } from './run'
 
 export function match<Ctx>(routers: string[], pathFactory: (ctx: Ctx) => string) {
-  return <T>(payloadGetter: (ctx: Ctx, skip: symbol) => T | Promise<T>) => {
+  return <T>(payloadGetter: (ctx: Ctx, skip: symbol) => symbol | T | Promise<T>) => {
     return function payloadGetterWithMatch(ctx: Ctx, skip: symbol) {
       const requestPath = pathFactory(ctx)
       if (requestPath && routers.some((router) => matchPath(router)(requestPath))) {
